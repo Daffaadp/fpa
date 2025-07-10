56fc85295f841a65355e2fe28c6067887c30b0fc
Arrofi20 <arrofi.rf20@gmail.com>
2025-07-10 11:03:54 +0700

    tambah controller tdl

--- src/ToDoListController.java
+++ src/ToDoListController.java
@@ -1,3 +1,56 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.event.ActionEvent;
+
 public class ToDoListController {
-    // Tambahkan logika event handler di sini jika diperlukan
+
+    @FXML
+    private ImageView homeImageView;
+
+    @FXML
+    private ImageView favoritImageView;
+
+    @FXML
+    private ImageView historyImageView;
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button favoritButton;
+
+    @FXML
+    private Button historyButton;
+
+    @FXML
+    public void initialize() {
+        try {
+            // Load gambar dari folder src/images
+            homeImageView.setImage(new Image(getClass().getResourceAsStream("images/Home.png")));
+            favoritImageView.setImage(new Image(getClass().getResourceAsStream("images/Favorit.png")));
+            historyImageView.setImage(new Image(getClass().getResourceAsStream("images/History.png")));
+        } catch (Exception e) {
+            System.out.println("Gagal memuat gambar: " + e.getMessage());
+        }
+    }
+
+    @FXML
+    private void handleHomeButtonAction(ActionEvent event) {
+        System.out.println("Navigasi ke Home");
+        // Tambahkan logika navigasi jika perlu
+    }
+
+    @FXML
+    private void handleFavoritButtonAction(ActionEvent event) {
+        System.out.println("Navigasi ke Favorit");
+        // Tambahkan logika navigasi jika perlu
+    }
+
+    @FXML
+    private void handleHistoryButtonAction(ActionEvent event) {
+        System.out.println("Navigasi ke History");
+        // Tambahkan logika navigasi jika perlu
+    }
 }
