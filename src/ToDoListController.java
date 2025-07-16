397128e02a6a0bc616a0f4b72800a2b7696beccf
Arrofi20 <arrofi.rf20@gmail.com>
2025-07-16 16:58:02 +0700

    Ganti Button TDT

--- src/ToDoListController.java
+++ src/ToDoListController.java
@@ -6,6 +6,11 @@ import javafx.scene.control.Alert.AlertType;
 public class ToDoListController {
 
     @FXML
+    private void handleDashboardMenu(ActionEvent event) {
+        showAlert("Navigasi", "Berpindah ke halaman Dashboard.");
+    }
+    
+    @FXML
     private void handleArtikelMenu(ActionEvent event) {
         showAlert("Navigasi", "Berpindah ke halaman Artikel.");
     }
