52306007616a8cf0b288b9ca4d28731b1f725313
Daffaadp <daffaaditya732@gmail.com>
2025-07-20 21:08:33 +0700

    test

---
+++ src/main/Main.java
@@ -0,0 +1,31 @@
+
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login.fxml"));
+            Scene scene = new Scene(loader.load());
+            
+            primaryStage.setTitle("Login System");
+            primaryStage.setScene(scene);
+            primaryStage.setWidth(800);
+            primaryStage.setFullScreen(false);
+            primaryStage.show();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
